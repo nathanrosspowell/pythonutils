585581f7ba92a2b33264acc5e675d37b30a57070
Nathan Ross Powell <nathanrosspowell@gmail.com>
2013-02-12 13:13:07 -0500

    [ADD] Word and number testing

--- FrenchNumbers.py
+++ FrenchNumbers.py
@@ -114,6 +114,26 @@ def getWord( number):
 def input( output ):
     return raw_input( output ).strip()
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Quiz a range.
+def quizRange( quizFunction, numberRange, trys = 3, shuffleThem = True ):
+    if shuffleThem:
+        shuffle( numberRange )
+    items = len( numberRange )
+    print "%s items in test:" % ( items, )
+    correct = 0
+    totalAttempts = 0
+    firstQ = True
+    for i, number in enumerate( numberRange ):
+        if not firstQ:
+            print "--------------"
+        firstQ = False;
+        print "%s) " % ( i + 1, ),
+        answer, attempts = quizFunction( number, trys )
+        totalAttempts += attempts
+        if answer:
+            correct += 1
+    print "Score: %d/%d, total attempts: %d" % ( correct, items, totalAttempts, )
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # Quiz a number.
 def quizNumber( number, trys = 3 ):
     word = getWord( number )
@@ -138,35 +158,47 @@ def quizNumber( number, trys = 3 ):
     print "'%s' is the number %d" % ( word, number, )
     return answer, totalAttempts
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-# Quiz a range .
-def quizNumberRange( numberRange, trys = 3, shuffleThem = True ):
-    if shuffleThem:
-        shuffle( numberRange )
-    items = len( numberRange )
-    print "%s items in test" % ( items, )
-    correct = 0
+# Quiz a word.
+def quizWord( number, trys = 3 ):
+    def cleanWord( theWord ):
+        return theWord.replace( '-', ' ' ).strip()
+    word = getWord( number )
+    print "What is the word for '%d'" % ( number, )
+    answer = False
     totalAttempts = 0
-    for i, number in enumerate( numberRange ):
-        print "%s) " % ( i + 1, ),
-        answer, attempts = quizNumber( number, trys )
-        totalAttempts += attempts
-        if answer:
-            correct += 1
-    print "Score: %d/%d, total attempts: %d" % ( correct, items, totalAttempts, )
+    while trys > 0 and answer is False:
+        totalAttempts += 1
+        answerWord = cleanWord( input( "Answer> " ) )
+        if answerWord == cleanWord( word ):
+            answer = True
+            print "Well done!"
+        else:
+            trys -= 1
+            print "Incorrect."
+            if trys > 0:
+                print "Please try again."
+    print "'%s' is the number %d" % ( word, number, )
+    return answer, totalAttempts
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-# A bunch of quiz functions
-def quizLowNumbers( trys = 3 ):
-    quizNumberRange( range( 20 ), trys )
-def quizTensNumbers( trys = 3 ):
-    quizNumberRange( range( 20 ), trys )
-def quizRandom( maxNumber, total, trys = 3 ):
+# A bunch of number quiz functions
+def quizLowNumbers( quizFunction, trys = 3 ):
+    quizRange( quizFunction, range( 20 ), trys )
+def quizTensNumbers( quizFunction, trys = 3 ):
+    quizRange( quizFunction,range( 20 ), trys )
+def quizRandom( quizFunction, maxNumber, total, trys = 3 ):
     nums = range( maxNumber + 1 )
     shuffle( nums )
     nums = [ x for i, x in enumerate( nums ) if i < total ]
-    quizNumberRange( nums, trys, shuffleThem = False )
+    quizRange( quizFunction, nums, trys, shuffleThem = False )
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Help.
+def printNumbers( min = 0, max = 20 ):
+    print "--------------\nNumbers %d to %d:" % ( min, max, )
+    for i in xrange( max + 1 ): 
+        print str( i ).rjust( 4 ), "=",  getWord( i )
+    print "--------------"
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # Main test.
 if __name__ == "__main__":
-    for i in xrange( 101 ): 
-        print str( i ).rjust( 4 ), "=",  getWord( i )
-    quizRandom( 5 )
+    printNumbers()
+    quizRandom( quizWord, 10, 3 )
