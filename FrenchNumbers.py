6a1e4288a6f1c4b8a2d8f8a3862c8e3994d2d76f
Nathan Ross Powell <nathanrosspowell@gmail.com>
2013-02-11 21:01:41 -0500

    [ADD] First pass of french words, still got errors

--- FrenchNumbers.py
+++ FrenchNumbers.py
@@ -1,82 +1,80 @@
-# If the numbers 1 to 5 are written out in words: one, two, three, four,
-# five, then there are 3 + 3 + 5 + 4 + 4 = 19 letters used in total.
-#
-# If all the numbers from 1 to 1000 (one thousand) inclusive were 
-# written out in words, how many letters would be used?
-#
-# NOTE: Do not count spaces or hyphens. For example, 342 (three hundred 
-# and forty-two) contains 23 letters and 115 (one hundred and fifteen) 
-# contains 20 letters. The use of "and" when writing out numbers is in 
-# compliance with British usage.
-
+#!/usr/bin/python
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# OutputProgress. Authored by Nathan Ross Powell.
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 units = (
-    "",
-    "one",
-    "two",
-    "three",
-    "four",
-    "five",
-    "six",
-    "seven",
-    "eight",
-    "nine",
-    "ten",
-    "eleven",
-    "twelve",
-    "thirteen",
-    "fourteen",
-    "fifteen",
-    "sixteen",
-    "seventeen",
-    "eighteen",
-    "nineteen",
+	"zero",
+	"un",
+	"deux",
+	"troi",
+	"quatre",
+	"cinq",
+	"six",
+	"sept",
+	"huit",
+	"neuf",
+	"dix",
+	"onze",
+	"douze",
+	"treize",
+	"quatorze",
+	"quinze",
+	"seize",
+	"dix-sept",
+	"dix-huit",
+	"dix-neuf",
 )
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 tens = (
-    "",
-    "",
-    "twenty",
-    "thirty",
-    "forty",
-    "fifty",
-    "sixty",
-    "seventy",
-    "eighty",
-    "ninety",
+	"",
+	"",
+	"vingt",
+	"trente",
+	"quarante",
+	"cinqanyte",
+	"sixante",
+	"soixante-dix",
+	"quatre-vingts",
+	"quatre-vingts-dix",
 )
-hundred = "hundred"
-thousand = "thousand"
-
+hundred = "cent"
+thousand = "mile"
+million = "million"
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Digit to word.
 def getWord( number):
-    strnum = str( number )[ :: -1 ]
-    word = ""
-    usedAnd = False
-    for i in range( len( strnum ) - 1, -1, -1 ):
-        n = int( strnum[ i ] )
-        if n == 0:
-            continue
-        if i == 3:
-            word += " %s %s" % ( units[ n ], thousand )
-        elif i == 2:
-            word += " %s %s" % ( units[ n ], hundred )
-        elif i == 1:
-            if len( word ) > 0 and not usedAnd: 
-                word += " and"
-                usedAnd = True 
-            if n > 1:
-                word += " %s" % ( tens[ n ], )
-            else:
-                n2 = int( strnum[ :2 ][ :: -1 ] )
-                word += " %s" % ( units[ n2 ], )
-                break
-        else:
-            if len( strnum ) > 2 and not usedAnd: 
-                word += " and"
-                usedAnd = True 
-            word += " %s" % ( units[ n ], )
-    return word
+	strnum = str( number )[ :: -1 ]
+	word = ""
+	usedAnd = False
+	for i in range( len( strnum ) - 1, -1, -1 ):
+		n = int( strnum[ i ] )
+		if n == 0:
+			continue
+		if i == 3:
+			word += " %s %s" % ( units[ n ], thousand )
+		elif i == 2:
+			word += " %s %s" % ( units[ n ], hundred )
+		elif i == 1:
+			if len( word ) > 0 and not usedAnd: 
+				word += " et"
+				usedAnd = True 
+			if n > 1:
+				word += " %s" % ( tens[ n ], )
+			else:
 
+				n2 = int( strnum[ :2 ][ :: -1 ] )
+				print "n2", n2
+				word += " %s" % ( units[ n2 ], )
+				break
+		else:
+			if len( strnum ) > 2 and not usedAnd: 
+				word += " and"
+				usedAnd = True 
+			word += " %s" % ( units[ n ], )
+	return word
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Main test.
 if __name__ == "__main__":
-    total = 0
-    for i in xrange( 1, 1001 ): 
-        total += len( getWord( i ).strip().replace( " ", "") )
-    print total
+	for i in xrange( 1, 1001 ): 
+		print getWord( i )
+
