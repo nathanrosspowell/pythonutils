20f122426fe9eaa1a3fd70de6dfe85ecf5cd4aa6
Nathan Ross Powell <nathanrosspowell@gmail.com>
2013-04-01 16:06:50 -0400

    [ADD] Windows Color change script
    Need to make it work on linux

---
+++ CmdColors.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# CmdColors. Authored by Nathan Ross Powell.
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Imports.
+import os
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+def dec2hex( n ):
+    """return the hexadecimal string representation of integer n"""
+    return "%X" % ( n, )
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+def hex2dec( s ):
+    """return the integer value of a hexadecimal string s"""
+    return int( s, 16 )
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+def setColor( a, b ):
+    string = "COLOR %s%s" % ( str( a ), str( b ), )
+    print string
+    os.system( string )
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+def loopColours():
+    for i in xrange( 16 ):
+        for j in range(16):
+            a = dec2hex( i )
+            b = dec2hex( j )
+            setColor( a, b )
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Test run.
+def cmdColoursTestRun():
+    print "Starting operation."
+    loopColours()
+    # Reset
+    setColor( 0, 7 )
+    print "\nOperation done!"
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Direct run of the file, invoke test.
+if __name__ == "__main__":
+    cmdColoursTestRun()
