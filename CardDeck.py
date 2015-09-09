8b99704888a5b2875d5d90961b28c97d4dce2b83
Nathan Ross Powell <nathanrosspowell@gmail.com>
2015-09-09 00:00:47 -0400

    Basic cards

--- CardDeck.py
+++ CardDeck.py
@@ -1,35 +1,26 @@
 #!/usr/bin/python
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-# CmdColors. Authored by Nathan Ross Powell.
+# CardDeck. Authored by Nathan Ross Powell.
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # Imports.
-import os
+from itertools import chain, repeat
+from random import shuffle
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-def dec2hex( n ):
-    """return the hexadecimal string representation of integer n"""
-    return "%X" % ( n, )
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-def hex2dec( s ):
-    """return the integer value of a hexadecimal string s"""
-    return int( s, 16 )
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-def setColor( a, b ):
-    string = "COLOR %s%s" % ( str( a ), str( b ), )
-    print(string)
-    os.system( string )
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-def loopColours():
-    for i in range( 16 ):
-        for j in range(16):
-            a = dec2hex( i )
-            b = dec2hex( j )
-            setColor( a, b )
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+def deck():
+    nums = list(chain(range(1,11), "JQKQ"));
+    fn = lambda i, x: "CSHS"[i] + str(x)
+    return [ fn(i, x) for i, n in enumerate(repeat(nums, 4)) for x in n ]
 # Test run.
 def cardDeckTestRun():
     print( "Starting operation.")
+    print("Full Deck:", list(deck()))
+    print("~~~")
+    to_shuffle = list(deck())
+    shuffle(to_shuffle)
+    print("Shuffled Deck:", to_shuffle)
     print("\nOperation done!")
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # Direct run of the file, invoke test.
 if __name__ == "__main__":
-    cardDesckTestRun()
+    cardDeckTestRun()
